50396070b6418f019b28b94725151feb7fd10727
PoojaGottipalli <poojagottipalli@gmail.com>
2024-06-24 23:54:25 +0530

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,5 @@
+const captchaTextBox=document.querySelector(".captcha input");
+const captchaInputBox=document.querySelector(".captcha-form input");
+const refreshButton=document.querySelector(".refresh button");
+const message=document.querySelector(".message");
+const message=document.querySelector(".message");
